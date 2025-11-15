6ebcb77bba86ac3a1544acefe5224b70a07c6fb6
maheenkamran <maheenkamrann14@gmail.com>
2025-11-15 16:56:04 +0500

    Voice to Text, Text to Voice

--- backend/chatbot/rag/config.py
+++ backend/chatbot/rag/config.py
@@ -1,11 +1,22 @@
+''' A config file (short for configuration file) is a file used to store settings or constants that your program 
+needs — things that don't usually change during execution.
+So instead of hardcoding these values in multiple places, you keep them in one file — 
+making your code cleaner, reusable, and easier to update. '''
+
 import os
+#provides functions to interact with the operating system.
+#Used here for working with file paths 
 
 # Get the absolute path to the project root (the main FastBot folder)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname((os.path.dirname(os.path.abspath(__file__))))))
+# os.path.abspath(__file__) gives full path of file
+# 4 times step to root folder 
 
-# File paths
-DATA_PATH = os.path.join(BASE_DIR, "docs", "QA.pdf")             # You can change to QA.json or QA.txt later
+DATA_PATH = os.path.join(BASE_DIR, "docs", "QA.pdf")  #joins path via \ like docs\QA.pdf
+           
 VECTOR_DB_PATH = os.path.join(BASE_DIR, "backend", "chatbot", "rag", "chroma_db")
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+#constants(caps+_) string storing name of our model
+
 QA_MODEL = "deepset/roberta-base-squad2"
--- backend/chatbot/rag/embeddings.py
+++ backend/chatbot/rag/embeddings.py
@@ -1,8 +1,19 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_embeddings_model(model_name):
-    """
-    Returns a LangChain embedding model compatible with Chroma.
-    """
+    
+    #Returns a LangChain embedding model compatible with Chroma.
     print(f"🔹 Loading embedding model: {model_name}")
     return HuggingFaceEmbeddings(model_name=model_name)
+
+'''
+Whoever calls get_embeddings_model(model_name) gets back a HuggingFaceEmbeddings object.
+Because it is an object it can access methods of its class.
+
+That object contains the loaded sentence-transformer model and provides special methods like:
+.embed_query(text) → turns a user's question into an embedding vector.
+.embed_documents(docs) → turns document text chunks into embeddings.
+
+When your RAG calls get_embeddings_model(...), it receives a ready-to-use embedding model 
+that can convert both questions and document text into vectors for similarity search.
+'''
